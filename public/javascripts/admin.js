25099f73c796b84f2e4760a53717a0fefc4d8700
hyeongyun0916 <mhg5303@gmail.com>
2018-06-09 04:06:16 +0900

    load hitter table for test

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,3 +1,43 @@
+function excelTest() {
+    $.ajax({
+        url: '/admin/tables/hitter',
+        type: 'get',
+        success: function (result) {
+            result = result.result;
+            console.log(result);
+            var columns = [];
+            var colHeaders = [];
+            result['tableInfo'].forEach(function (element) {
+                var column = {};
+                colHeaders.push(element['Field']);
+                if (element['Type'].startsWith('int')) {
+                    column.type = 'int';
+                } else if (element['Type'].startsWith('varchar')) {
+                    column.type = 'text';
+                }
+                columns.push(column);
+            });
+            var data = [];
+            result['tableRows'].forEach(function (element) {
+                var row = [];
+                colHeaders.forEach(function (field) {
+                    row.push(element[field]);
+                });
+                data.push(row);
+            });
+            $('#table').jexcel({
+                data: data,
+                colHeaders: colHeaders,
+                // colWidths: [300, 80, 100, 60, 120],
+                columns: columns
+            });
+        },
+        error: function (result) {
+            console.log(result);
+        }
+    });
+}
+
 $(function () {
     $.ajax({
         url: '/admin/tables',
@@ -14,4 +54,5 @@ $(function () {
             })
         }
     });
+    excelTest();
 });
--- routes/admin.js
+++ routes/admin.js
@@ -35,4 +35,40 @@ router.get('/tables', function (req, res, next) {
   });
 });
 
+router.get('/tables/:table', function (req, res, next) {
+  var tableInfo, tableRows;
+  dbModule.withConnection(dbModule.pool, function (connection, next) {
+    connection.query('describe ' + req.params.table, function (err, tableInfoP) {
+      if (err) {
+        return next(err, 'DESCRIBE table error.');
+      }
+      tableInfo = tableInfoP;
+      connection.query('select * from ' + req.params.table, function (err, tableRowsP) {
+        if (err) {
+          return next(err, 'SELECT table error');
+        }
+        tableRows = tableRowsP;
+        return next(err);
+      });
+    });
+  }, function (err, message) {
+    if (err) {
+      res.status(400).json({
+        'code': -1,
+        'msg': 'query error',
+        'result': err
+      });
+    } else {
+      res.status(200).json({
+        'code': 0,
+        'msg': 'suc',
+        'result': {
+          tableInfo: tableInfo,
+          tableRows: tableRows
+        }
+      });
+    }
+  });
+});
+
 module.exports = router;
