bf2c6401e332b9badad7d0659b2d0a5cc7e17b7e
hyeongyun0916 <mhg5303@gmail.com>
2018-06-14 00:21:39 +0900

    modify agent-1 when team Changed

--- public/javascripts/agent-1.js
+++ public/javascripts/agent-1.js
@@ -1,61 +1,126 @@
-var teamID = getUrlVars()['teamID'];
+function getHitter(teamID) {
+    $.ajax({
+        url: '/agent-1/team_hitter_record',
+        type: 'get',
+        data: {
+            'teamID': teamID
+        },
+        success: function (json) {
+            $('#team_hitter').DataTable({
+                data: json.result,
+                "columns": [{
+                        "data": "NAME"
+                    },
+                    {
+                        "data": "PA"
+                    },
+                    {
+                        "data": "AB"
+                    },
+                    {
+                        "data": "DOBLE"
+                    },
+                    {
+                        "data": "TRIPLE"
+                    },
+                    {
+                        "data": "SB"
+                    },
+                    {
+                        "data": "CS"
+                    },
+                    {
+                        "data": "SH"
+                    },
+                    {
+                        "data": "SF"
+                    },
+                    {
+                        "data": "SALARY"
+                    },
+                    {
+                        "data": "AVG"
+                    },
+                    {
+                        "data": null,
+                        defaultContent: "<button onclick=''>Click!</button>"
+                    }
+                ]
+            });
+        }
+    });
+}
 
-$.ajax({
-    url:'/agent-1/team_hitter_record',
-    type: 'get',
-    data:{
-        'teamID' : teamID
-    },
-    success: function(json){
-        $('#team_hitter').DataTable({
-            data: json.result,
-            "columns": [
-                { "data": "NAME" },
-                { "data": "PA" },
-                { "data": "AB" },
-                { "data": "DOBLE" },
-                { "data": "TRIPLE" },
-                { "data": "SB" },
-                { "data": "CS" },
-                { "data": "SH" },
-                { "data": "SF" },
-                { "data": "SALARY" },
-                { "data": "AVG" },
-                { "data": null,
-            defaultContent:"<button onclick=''>Click!</button>"}
-            ]
-        });
-    }
-})
+function getPitcher(teamID) {
+    $.ajax({
+        url: '/agent-1/team_pitcher_record',
+        type: 'get',
+        data: {
+            'teamID': teamID
+        },
+        success: function (json) {
+            $('#team_pitcher').DataTable({
+                data: json.result,
+                "columns": [{
+                        "data": "NAME"
+                    },
+                    {
+                        "data": "WIN"
+                    },
+                    {
+                        "data": "LOSE"
+                    },
+                    {
+                        "data": "SAVE"
+                    },
+                    {
+                        "data": "HOLD"
+                    },
+                    {
+                        "data": "ER"
+                    },
+                    {
+                        "data": "IP"
+                    },
+                    {
+                        "data": "CG"
+                    },
+                    {
+                        "data": "H"
+                    },
+                    {
+                        "data": "NP"
+                    },
+                    {
+                        "data": "ERA"
+                    },
+                    {
+                        "data": "DOBLE"
+                    },
+                    {
+                        "data": "TRIPLE"
+                    },
+                    {
+                        "data": "G"
+                    },
+                    {
+                        "data": "SALARY"
+                    },
+                    {
+                        "data": null,
+                        defaultContent: "<button onclick=''>Click!</button>"
+                    }
+                ]
+            });
+        }
+    });
+}
 
-$.ajax({
-    url:'/agent-1/team_pitcher_record',
-    type: 'get',
-    data:{
-        'teamID' : teamID
-    },
-    success: function(json){
-        $('#team_pitcher').DataTable({
-            data: json.result,
-            "columns": [
-                { "data": "NAME" },
-                { "data": "WIN" },
-                { "data": "LOSE" },
-                { "data": "SAVE" },
-                { "data": "HOLD" },
-                { "data": "ER" },
-                { "data": "IP" },
-                { "data": "CG" },
-                { "data": "H" },
-                { "data": "NP" },
-                { "data": "ERA" },
-                { "data": "DOBLE" },
-                { "data": "TRIPLE" },
-                { "data": "G" },
-                { "data": "SALARY" },
-                { "data": null,
-            defaultContent:"<button onclick=''>Click!</button>"}
-            ]
-        });
-    }
-})
+$(function () {
+    var teamID = getUrlVars()['teamID'];
+    getPitcher(teamID);
+    getHitter(teamID);
+
+    selectableFunctionCallback.push(getPitcher);
+    selectableFunctionCallback.push(getHitter);
+});
